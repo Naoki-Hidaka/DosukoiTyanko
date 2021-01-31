f1ced0ae17b9f2ccf098d74bc093730cdbf6c2f4
日髙 尚輝 <naoki.h@arsaga.jp>
2021-01-31 23:44:08 +0900

    ライブラリを追加

---
+++ app/src/main/java/com/example/myapplication/dosukoityanko/presentation/view/detailRestaurant/DetailRestaurantFragment.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.dosukoityanko.presentation.view.detailRestaurant
+
+import androidx.fragment.app.Fragment
+
+class DetailRestaurantFragment : Fragment() {
+}
---
+++ app/src/main/java/com/example/myapplication/dosukoityanko/presentation/view/restaurantList/RestaurantListFragment.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.dosukoityanko.presentation.view.restaurantList
+
+import androidx.fragment.app.Fragment
+
+class RestaurantListFragment : Fragment() {
+}
