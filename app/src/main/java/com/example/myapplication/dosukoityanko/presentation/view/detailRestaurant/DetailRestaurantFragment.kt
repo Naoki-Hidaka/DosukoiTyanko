4505cef7846fdd56892266a4afa79933e271a064
日髙 尚輝 <naoki.h@arsaga.jp>
2021-02-01 17:54:15 +0900

    詳細ページを作成

--- app/src/main/java/com/example/myapplication/dosukoityanko/presentation/view/detailRestaurant/DetailRestaurantFragment.kt
+++ app/src/main/java/com/example/myapplication/dosukoityanko/presentation/view/detailRestaurant/DetailRestaurantFragment.kt
@@ -1,6 +1,36 @@
 package com.example.myapplication.dosukoityanko.presentation.view.detailRestaurant
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import com.example.myapplication.dosukoityanko.databinding.FragmentDetailRestaurantBinding
 
 class DetailRestaurantFragment : Fragment() {
+
+    companion object {
+        private const val webViewUrl = "https://r.gnavi.co.jp/p458303/?ak=KxDKTtl%2BGwknQP4k%2Fpbc%2Bxt8hrFxrOsZwIF9HhekV8g%3D"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentDetailRestaurantBinding.inflate(inflater, container, false).let {
+        it.lifecycleOwner = viewLifecycleOwner
+        it.webView.apply {
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    it.progressBar.visibility = View.GONE
+                }
+            }
+            settings.supportZoom()
+            settings.builtInZoomControls = true
+            loadUrl(webViewUrl)
+        }
+        it.root
+    }
 }
